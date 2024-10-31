7a58782b76d1f5760e08778f0fe47ac1fce4c32e
Süphan <yakutsuphan@gmail.com>
2024-10-31 21:10:52 +0300

    Routing integration is done.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import {Route,Routes} from 'react-router-dom'
+import {Home} from './components/Home'
+import {Menu} from './components/Menu'
+import {About} from './components/About'
+import {Contact} from './components/Contact'
+
+
+function App() {
+
+  return (
+    <div className='App'>
+     <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/menu' element={<Menu/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/contact' element={<Contact/>} />
+     </Routes>
+
+
+    </div>
+  
+    
+  )
+}
+
+export default App
