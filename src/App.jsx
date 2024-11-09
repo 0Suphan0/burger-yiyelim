c4a4da8f74fb5dcab8aecd82742907659dddf157
Süphan <yakutsuphan@gmail.com>
2024-11-09 20:57:35 +0300

    Asset's added and navbar is created.

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import {Home} from './components/Home'
 import {Menu} from './components/Menu'
 import {About} from './components/About'
 import {Contact} from './components/Contact'
+import Navbar from './components/Navbar'
 
 
 function App() {
 
   return (
     <div className='App'>
-     <Routes>
+    <Navbar/>
+
+    <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/menu' element={<Menu/>} />
       <Route path='/about' element={<About/>} />
       <Route path='/contact' element={<Contact/>} />
-     </Routes>
+    </Routes>
 
 
     </div>
