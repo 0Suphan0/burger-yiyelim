f5798add5622c4ad03e580065369b384a0ec93f4
Süphan <yakutsuphan@gmail.com>
2024-11-09 22:22:45 +0300

    Menu page is done

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import {Data} from '../helpers/Data'
+import '../style/menu.css'
+import MenuItem from '../components/MenuItem'
 
 export const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='menu'>
+      <h1 className="menuTitle">Burgerlerimiz</h1>
+      <div className="menuList">
+        {Data.map((menuItem,key)=>{
+          return <MenuItem
+          key={key}
+          image={menuItem.image}
+          name={menuItem.name}
+          content={menuItem.content}
+          price={menuItem.price}
+          
+          />
+        })}
+      </div>
+    </div>
   )
 }
---
+++ src/helpers/Data.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import BurgerPhoto from '../assets/burger.jpg'
+
+export const Data = [
+    {
+        name:"Efso Burger",
+        image:BurgerPhoto,
+        content:'150 gr Kasap Burger,Patates,Turşu',
+        price:250
+    },
+    {
+        name:"Efso Burger",
+        image:BurgerPhoto,
+        content:'150 gr Kasap Burger,Patates,Turşu',
+        price:250
+    },
+    {
+        name:"Efso Burger",
+        image:BurgerPhoto,
+        content:'150 gr Kasap Burger,Patates,Turşu',
+        price:250
+    },
+    {
+        name:"Efso Burger",
+        image:BurgerPhoto,
+        content:'150 gr Kasap Burger,Patates,Turşu',
+        price:250
+    },
+    {
+        name:"Efso Burger",
+        image:BurgerPhoto,
+        content:'150 gr Kasap Burger,Patates,Turşu',
+        price:250
+    },
+    {
+        name:"Efso Burger",
+        image:BurgerPhoto,
+        content:'150 gr Kasap Burger,Patates,Turşu',
+        price:250
+    },
+    {
+        name:"Efso Burger",
+        image:BurgerPhoto,
+        content:'150 gr Kasap Burger,Patates,Turşu',
+        price:250
+    },
+    {
+        name:"Efso Burger",
+        image:BurgerPhoto,
+        content:'150 gr Kasap Burger,Patates,Turşu',
+        price:250
+    },
+    {
+        name:"Efso Burger",
+        image:BurgerPhoto,
+        content:'150 gr Kasap Burger,Patates,Turşu',
+        price:250
+    },
+    {
+        name:"Efso Burger",
+        image:BurgerPhoto,
+        content:'150 gr Kasap Burger,Patates,Turşu',
+        price:250
+    },
+    {
+        name:"Efso Burger",
+        image:BurgerPhoto,
+        content:'150 gr Kasap Burger,Patates,Turşu',
+        price:250
+    },
+    {
+        name:"Efso Burger",
+        image:BurgerPhoto,
+        content:'150 gr Kasap Burger,Patates,Turşu',
+        price:250
+    },
+    
+
+]
