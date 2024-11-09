a73fb995991879e0982379e0a0fbaddff66701f3
Süphan <yakutsuphan@gmail.com>
2024-11-09 21:30:48 +0300

    Home page is done.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Banner from '../assets/banneryeni.jpg'
+import '../style/home.css'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='mainPage' style={{backgroundImage: `url(${Banner})`}}>
+      <div className="order">
+        <Link to="/menu">
+          <button>Sipariş Ver</button>
+        </Link>
+      </div>
+    </div>  
   )
 }
