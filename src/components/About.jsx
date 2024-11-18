3f59f178ddb1d0e8cb6bd8df2546ac53d1e37b3c
Süphan <yakutsuphan@gmail.com>
2024-11-18 23:06:10 +0300

    about us page is done.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import BannerImage from '../assets/banneryeni.jpg'
+import '../style/about.css'
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+
+      <div className='aboutTop' style={{backgroundImage:`url(${BannerImage})`}}>
+
+      </div>
+      <div className="aboutBottom">
+        <h1>Hakkımızda</h1>
+        <p>Lorem 
+          ipsumdolor sit amet, consectetur adipisicing elit. Animi delectus provident dicta recusandae perferendis facilis nesciunt dolorum neque non dolorem vitae similique deserunt eum reprehenderit, in nemo porro tempora tenetur.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem, voluptatum. Modi eos cum blanditiis voluptas ea, minima commodi esse eum consequuntur adipisci. Error quibusdam soluta quod deleniti repellat, optio esse?
+        </p>
+
+      </div>
+
+    </div>
   )
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,6 +6,7 @@ import MenuItem from '../components/MenuItem'
 export const Menu = () => {
   return (
     <div className='menu'>
+      <br />
       <h1 className="menuTitle">Burgerlerimiz</h1>
       <div className="menuList">
         {Data.map((menuItem,key)=>{
